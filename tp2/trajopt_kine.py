dda025c51e6aaf4374c6cb0cf9a2487707cf0f33
Mansard <nmansard@laas.fr>
2023-07-02 16:55:30 +0200

    First part of notebook #2.

--- tp2/trajopt_kine.py
+++ tp2/trajopt_kine.py
@@ -12,6 +12,7 @@ The following tools are used:
 - the meshcat viewer
 '''
 
+# %jupyter_snippet imports
 import time
 import unittest
 import numpy as np
@@ -23,35 +24,44 @@ from scipy.optimize import fmin_bfgs
 from numpy.linalg import norm
 
 from utils.meshcat_viewer_wrapper import MeshcatVisualizer
+# %end_jupyter_snippet
+
 
 # Change numerical print
 pin.SE3.__repr__=pin.SE3.__str__
 np.set_printoptions(precision=2, linewidth=300, suppress=True,threshold=1e6)
 
 ### HYPER PARAMETERS
+# %jupyter_snippet configurations
 Mtarget = pin.SE3(pin.utils.rotate('y', 3), np.array([-0.5, 0.1, 0.2]))  # x,y,z
 q0 = np.array([0, -3.14 / 2, 0, 0, 0, 0])
+endEffectorFrameName = 'tool0'
+# %end_jupyter_snippet 
+# %jupyter_snippet hyper
 T = 10
 w_run = .1
 w_term = 1
+# %end_jupyter_snippet
 
 # --- Load robot model
-robot = robex.load('ur5')
+robot = robex.load('ur10')
+# %jupyter_snippet modeldata
 robot.q0 = q0
-# Open the viewer
-viz = MeshcatVisualizer(robot)
-viz.display(robot.q0)
 
 # The pinocchio model is what we are really interested by.
 model = robot.model
 data = model.createData()
-idTool = model.getFrameId('tool0')
+endEffector_ID = model.getFrameId(endEffectorFrameName)
+# %end_jupyter_snippet
 
 # --- Add box to represent target
+# %jupyter_snippet viewer
+# Open the viewer
+viz = MeshcatVisualizer(robot)
+viz.display(robot.q0)
 # Add a vizualization for the target
 boxID = "world/box"
 viz.addBox(boxID, [.05, .1, .2], [1., .2, .2, .5])
-# %jupyter_snippet 1
 # Add a vizualisation for the tip of the arm.
 tipID = "world/blue"
 viz.addBox(tipID, [.08] * 3, [.2, .2, 1., .5])
@@ -59,40 +69,59 @@ def displayScene(q,dt=1e-1):
     '''
     Given the robot configuration, display:
     - the robot
-    - a box representing idTool
+    - a box representing endEffector_ID
     - a box representing Mtarget
     '''
     pin.framesForwardKinematics(model,data,q)
-    M = data.oMf[idTool]
+    M = data.oMf[endEffector_ID]
     viz.applyConfiguration(boxID, Mtarget)
     viz.applyConfiguration(tipID, M)
     viz.display(q)
     time.sleep(dt)
+# %end_jupyter_snippet
+# %jupyter_snippet disptraj
 def displayTraj(qs,dt=1e-2):
     for q in qs[1:]:
         displayScene(q,dt=dt)
+# %end_jupyter_snippet
 
+# %jupyter_snippet casadi
 # --- Casadi helpers
 cmodel = cpin.Model(model)
 cdata = cmodel.createData()
 
 cq = casadi.SX.sym("x",model.nq,1)
 cpin.framesForwardKinematics(cmodel,cdata,cq)
-pos_tool = casadi.Function('ptool', [cq], [ cdata.oMf[idTool].translation ])
-error_tool = casadi.Function('etool', [cq],
-                             [ cpin.log6(cdata.oMf[idTool].inverse() * cpin.SE3(Mtarget)).vector ])
+
+error3_tool = casadi.Function('etool3', [cq],
+                              [ cdata.oMf[endEffector_ID].translation - Mtarget.translation ])
+error6_tool = casadi.Function('etool6', [cq],
+                             [ cpin.log6(cdata.oMf[endEffector_ID].inverse() * cpin.SE3(Mtarget)).vector ])
+error_tool = error3_tool
+# %end_jupyter_snippet
 
 ### PROBLEM
+# %jupyter_snippet casadi_opti
 opti = casadi.Opti()
 var_qs = [ opti.variable(model.nq) for t in range(T+1) ]
 totalcost = 0
-opti.subject_to(var_qs[0] == robot.q0)
+# %end_jupyter_snippet
 
+# %jupyter_snippet casadi_q0
+opti.subject_to(var_qs[0] == robot.q0)
+# %end_jupyter_snippet
+
+# %jupyter_snippet casadi_runcost
 for t in range(T):
     totalcost += w_run * casadi.sumsqr( var_qs[t] - var_qs[t+1] )
+# %end_jupyter_snippet
+
+# %jupyter_snippet casadi_termcost
 totalcost += w_term * casadi.sumsqr( error_tool(var_qs[T]) )
+# %end_jupyter_snippet
 
 ### SOLVE
+# %jupyter_snippet solve
 opti.minimize(totalcost)
 opti.solver("ipopt") # set numerical backend
 opti.callback(lambda i: displayScene(opti.debug.value(var_qs[-1])))
@@ -105,6 +134,7 @@ try:
 except:
     print('ERROR in convergence, plotting debug info.')
     sol_qs = [ opti.debug.value(var_q) for var_q in var_qs ]
+# %end_jupyter_snippet
 
 print("***** Display the resulting trajectory ...")
 displayScene(robot.q0,1)
