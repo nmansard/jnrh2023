7160cb12cda897ebfdde4420751a1039d2952761
Stéphane Caron <stephane.caron@normalesup.org>
2023-07-04 18:45:34 +0200

    Ongoing script updates

--- tp1/invgeom.py
+++ tp1/invgeom.py
@@ -19,7 +19,7 @@ import time
 import unittest
 
 import example_robot_data as robex
-import meshcat_shapes
+import meshcat
 import numpy as np
 import pinocchio as pin
 from numpy.linalg import norm
@@ -70,22 +70,78 @@ def error(q: np.ndarray) -> float:
 # %end_jupyter_snippet
 
 
-# --- Callback for visualization
+# %jupyter_snippet meshcat_frame
+def meshcat_frame(
+    handle: meshcat.Visualizer,
+    axis_length: float = 0.1,
+    axis_thickness: float = 0.005,
+    opacity: float = 1.0,
+    origin_color: int = 0x000000,
+    origin_radius: float = 0.01,
+) -> None:
+    """
+    Set MeshCat handle to a frame, represented by an origin and three axes.
+
+    Args:
+        handle: MeshCat handle to attach the frame to.
+        axis_length: Length of axis unit vectors, in [m].
+        axis_thickness: Thickness of axis unit vectors, in [m].
+        opacity: Opacity of all three unit vectors.
+        origin_color: Color of the origin sphere.
+        origin_radius: Radius of the frame origin sphere, in [m].
+
+    Note:
+        As per the de-facto standard (Blender, OpenRAVE, RViz, ...), the
+        x-axis is red, the y-axis is green and the z-axis is blue.
+    """
+    material = meshcat.geometry.MeshLambertMaterial(
+        color=origin_color, opacity=opacity
+    )
+    sphere = meshcat.geometry.Sphere(origin_radius)
+    handle.set_object(sphere, material)
+    direction_names = ["x", "y", "z"]
+    colors = [0xFF0000, 0x00FF00, 0x0000FF]
+    rotation_axes = [[0, 0, 1], [0, 1, 0], [1, 0, 0]]
+    position_cylinder_in_body = 0.5 * axis_length * np.eye(3)
+    for i in range(3):
+        dir_name = direction_names[i]
+        material = meshcat.geometry.MeshLambertMaterial(
+            color=colors[i], opacity=opacity
+        )
+        transform_cylinder_to_body = meshcat.transformations.rotation_matrix(
+            np.pi / 2, rotation_axes[i]
+        )
+        transform_cylinder_to_body[0:3, 3] = position_cylinder_in_body[i]
+        cylinder = meshcat.geometry.Cylinder(axis_length, axis_thickness)
+        handle[dir_name].set_object(cylinder, material)
+        handle[dir_name].set_transform(transform_cylinder_to_body)
+
+
+# %end_jupyter_snippet
+
+# %jupyter_snippet visualizer_callback
 viewer = viz.viewer
-meshcat_shapes.frame(viewer["target"], opacity=1.0)
-meshcat_shapes.frame(viewer["current"], opacity=0.5)
+
+# Target frame (immobile)
+meshcat_frame(viewer["target"], opacity=1.0)
+viewer["target"].set_transform(transform_target_to_world.np)
+
+# Tool frame (mobile)
+meshcat_frame(viewer["tool"], opacity=0.5)
 
 
 def callback(q: np.ndarray):
     pin.framesForwardKinematics(model, data, q)
     transform_frame_to_world = data.oMf[tool_id]
-    viewer["target"].set_transform(transform_target_to_world.np)
-    viewer["current"].set_transform(transform_frame_to_world.np)
+    viewer["tool"].set_transform(transform_frame_to_world.np)
     viz.display(q)
     time.sleep(1e-1)
 
 
-qguess = robot.q0
+# %end_jupyter_snippet
+
+
+# %jupyter_snippet fmin_bfgs
 qguess = np.array([0.12, -2.2, -1.45, 1.82, -0.95, 0.17])
 qopt = fmin_bfgs(error, qguess, callback=callback)
 
@@ -93,6 +149,7 @@ print(
     "The robot finally reached effector placement at\n",
     robot.placement(qopt, 6),
 )
+# %end_jupyter_snippet
 
 # TEST ZONE ############################################################
 # Some asserts below to check the behavior of this script in stand-alone
@@ -121,7 +178,7 @@ class InvGeom6DTest(unittest.TestCase):
 
 InvGeom6DTest().test_qopt_6d()
 
-##
+# == End of test zone ==
 
 # %jupyter_snippet casadi_imports
 import casadi
